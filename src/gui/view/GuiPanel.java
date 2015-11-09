44fbb469c6ef39d498b72e0aacba94577a25c775
AlexHuntsman <ahun0442@740s-pl-112.local>
2015-11-09 09:55:37 -0700

    Started the layout

--- src/gui/view/GuiPanel.java
+++ src/gui/view/GuiPanel.java
@@ -3,6 +3,7 @@ package gui.view;
 import javax.swing.JPanel;
 import javax.swing.JButton;
 import javax.swing.JTextField;
+import javax.swing.SpringLayout;
 import gui.controller.GuiController;
 /**
  * 
@@ -15,14 +16,17 @@ public class GuiPanel extends JPanel
 	private GuiController baseController;
 	private JButton firstButton;
 	private JTextField firstTextField;
+	private SpringLayout baseLayout;
 	
 	
 	public GuiPanel (GuiController baseController)
 	{
 		this.baseController = baseController;
 		
+		baseLayout = new SpringLayout();
 		firstButton = new JButton("Pointless Button");
 		firstTextField = new JTextField("words can be typed here");
+		
 		
 		setupPanel();
 		setupLayout();
@@ -31,11 +35,17 @@ public class GuiPanel extends JPanel
 	
 	private void setupPanel()
 	{
-		
+		this.setLayout(baseLayout);
+		this.add(firstButton);
+		this.add(firstTextField);
 	}
 	
-	private void setupLayout()
+	private void setupLayout() //where the code fromthe design tab gos.
 	{
+		baseLayout.putConstraint(SpringLayout.EAST, firstButton, -148, SpringLayout.EAST, this);
+		baseLayout.putConstraint(SpringLayout.NORTH, firstButton, 43, SpringLayout.SOUTH, firstTextField);
+		baseLayout.putConstraint(SpringLayout.NORTH, firstTextField, 41, SpringLayout.NORTH, this);
+		baseLayout.putConstraint(SpringLayout.EAST, firstTextField, 0, SpringLayout.EAST, this);
 		
 	}
 	
